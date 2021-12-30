07724cdf0860dcb7398a204f9963f6de6d3f5127
Bryan Vela Alvarado <bdvela20@gmail.com>
2021-12-30 01:28:16 -0500

    fix: photos

--- js/main.js
+++ js/main.js
@@ -81,3 +81,25 @@ $(window)
     );
   });
 })();
+
+$(document).ready(function () {
+  var referencePosition = 0; // donde se almacena a partir de que top se muestra el navbar
+  var lastPosition = 0; // Auxiliar para saber si estas bajando o subiendo
+
+  $(window).scroll(function () {
+    console.log($(this).scrollTop(), referencePosition);
+    let positionActual = $(this).scrollTop();
+    if (positionActual > referencePosition) {
+      //si estas debajo del punto de referencia, ocultar
+      $('.navbar').fadeOut(150);
+    } else {
+      // si subistes arriba del punto de referencia mostrar
+      $('.navbar').fadeIn(150);
+    }
+    if (positionActual > lastPosition) {
+      // si estas bajando, actualizar punto de referencia a 50 pixeles arriba del scroll actual
+      referencePosition = positionActual - 50;
+    }
+    lastPosition = positionActual; // actualizar auxiliar para saber si subes o bajas
+  });
+});
